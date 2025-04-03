f2e6e8c4a938f841323018f63b6f51701a20aec4
Jamal Shah Gabar <jamalshahgabar@Jamals-MacBook-Pro.local>
2025-04-04 00:41:32 +0100

    Fix: proper index.js with SpeedInsights and clean comment

--- src/index.js
+++ src/index.js
@@ -3,15 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <SpeedInsights /> {/* ✅ TRACKS performance */}
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// ✅ If you want to start measuring performance, pass a function (e.g. console.log)
+// Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
