a6db1c4aeebe49b5932555be8507257335d2c703
Jamal Shah Gabar <jamalshahgabar@Jamals-MacBook-Pro.local>
2025-04-04 00:42:49 +0100

    Cleanup: removed unused reportWebVitals

--- src/index.js
+++ src/index.js
@@ -2,17 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { SpeedInsights } from '@vercel/speed-insights/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <SpeedInsights /> {/* ✅ TRACKS performance */}
+    <SpeedInsights />
   </React.StrictMode>
 );
-
-// ✅ If you want to start measuring performance, pass a function (e.g. console.log)
-// Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
